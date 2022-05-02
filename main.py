25dabf03b3029b65dca51ccb15fbeb94619cbae9
alcyada <herokuwar4@gmail.com>
2022-05-02 16:19:24 +0000

    hi

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ vae = get_vae(dwt=True).to(device)
 realesrgan = get_realesrgan('x2', device=device)
 clip, processor = ruclip.load('ruclip-vit-base-patch32-384', device=device)
 clip_predictor = ruclip.Predictor(clip, processor, device, bs=8)
- scores = []
+scores = []
 @app.options("/nft/{query}")
 async def read_i(query : str):
    return {"query": success}
@@ -35,8 +35,8 @@ async def read_item(query : str):
  for top_k, top_p, images_num in [
     (2048, 0.995, 24),
 ]:
-    _pil_images, _scores = generate_images(text, tokenizer, dalle, vae, top_k=top_k, images_num=images_num, bs=8, top_p=top_p)
+    _pil_images, _scores =await generate_images(text, tokenizer, dalle, vae, top_k=top_k, images_num=images_num, bs=8, top_p=top_p)
     pil_images += _pil_images
     scores += _scores
-  return {"query": pil_images[69]}
+ return {"query": pil_images[69]}
 
